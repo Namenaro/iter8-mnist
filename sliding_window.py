63c4ac17eef7aa8bbe79363bd86237c18a4c7951
yana <nanenaro@gmail.com>
2021-05-29 23:45:45 +0300

    dd

---
+++ sliding_window.py
@@ -0,0 +1,32 @@
+def check_dispersia(matrix):
+    pass
+
+def check_mean(matrix):
+    pass
+
+def check_max_perepad(matrix):
+    pass
+
+def check_sum_of_derivatives(matrix):
+    pass
+
+def slide(pic, winside):
+    pass
+
+def get_statistics_on_descriptor(pics):
+    #for every pic in pics
+    # make slide
+    # and sum all
+    # and make hist on activations of descr
+
+    # top descriptirs which are more informative????
+    pass
+
+def loose_information(matrix):
+    pass
+
+def is_matrix_homogene(matrix):
+    pass
+
+def try_predict_in_vicinity_of_triggering_decriptor():
+    pass
