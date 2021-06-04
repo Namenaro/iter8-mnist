d0becbdb144a3a5ce731eeb287ff52db4937dcf0
neuro <nanenaro@gmail.com>
2021-06-04 12:02:41 +0300

    selector of start point

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from utils import *
 from slider import *
+from select_coord import select_coord_on_pic
+
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -7,7 +9,7 @@ import matplotlib.pyplot as plt
 if __name__ == "__main__":
     #pics = get_numbers_of_type(the_number=2)
     #print(pics.shape)
-    side = 15
+    side = 5
     checker = check_dispersion
     #pic = pics[6]
     #pic = np.pad(pic, side, mode='constant', constant_values=0)
@@ -15,5 +17,7 @@ if __name__ == "__main__":
     #pic2 = slide(pic, side, checker)
     #show_2_gray_pics(pic, pic2)
 
-    pics = get_numbers_of_type(the_number=9)[0:50]
-    get_stat(pics, side, checker)
+    pics = get_numbers_of_type(the_number=9)[0:90]
+    #get_stat(pics, side, checker)
+    x,y = select_coord_on_pic(pics[0])
+    print(x)
